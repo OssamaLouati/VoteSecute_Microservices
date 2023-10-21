b6ec47dfb4c1315650a0d45962d40986610d0a84
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-10-21 17:07:21 +0100

    create a form for tesing the auth

--- client-ui/src/App.js
+++ client-ui/src/App.js
@@ -1,24 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Routes>
+            {/* Route for the Login page */}
+            <Route path="/login" element={<Login />} />
+            
+            {/* Default route for the Home page */}
+            <Route path="/" element={<Login/>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
