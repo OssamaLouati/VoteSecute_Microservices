d75f395785185e6e14db7893e49662654efe5419
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-10-21 17:16:21 +0100

    setup architecture for auth service

---
+++ auth-service/config/jwt.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secret: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NzkwNDgyNiwiaWF0IjoxNjk3OTA0ODI2fQ.TLBQBs-zUZEe5xZZSlukBptnenRlVBGLGzxFhyW8GiQ', 
+    expiresIn: '1h', // Token expiration time
+  };
+  
