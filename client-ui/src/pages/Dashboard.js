aedff4f7de32999fdd622d9f92c2cf17e9b05a24
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-10-28 20:27:55 +0100

    added showing winners

--- client-ui/src/pages/Dashboard.js
+++ client-ui/src/pages/Dashboard.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell, ResponsiveContainer
+} from 'recharts';
 
 function Results() {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [showWinners, setShowWinners] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:7000/results')
@@ -19,25 +22,63 @@ function Results() {
         return <p>Loading...</p>;
     }
 
+    const renderColorfulBar = (entry, highestVote) => {
+        return entry.votes_number === highestVote ? '#FF4500' : '#8884d8';
+    };
+
     return (
         <div>
-            {data.map((result, index) => (
-                <div key={index}>
-                    <h2>Role: {result.role}</h2>
-                    <ResponsiveContainer width="100%" height={400}>
-                        <BarChart 
-                            data={result.candidates} 
-                            margin={{top: 15, right: 30, left: 20, bottom: 5}}>
-                            <CartesianGrid strokeDasharray="3 3"/>
-                            <XAxis dataKey="name"/>
-                            <YAxis/>
-                            <Tooltip/>
-                            <Legend/>
-                            <Bar dataKey="votes_number" fill="#8884d8" />
-                        </BarChart>
-                    </ResponsiveContainer>
-                </div>
-            ))}
+            <button onClick={() => setShowWinners(!showWinners)}>
+                {showWinners ? 'Go back to statistics' : 'Show winners'}
+            </button>
+
+            {showWinners ? (
+                data.map((result, index) => {
+                  const totalVotes = result.candidates.reduce((total, candidate) => total + candidate.votes_number, 0);
+                  const highestVote = Math.max(...result.candidates.map(candidate => candidate.votes_number));
+                  const winners = result.candidates.filter(candidate => candidate.votes_number === highestVote);
+                  const winnerPercentage = (highestVote / totalVotes * 100).toFixed(2);
+                  return (
+                      <div key={index}>
+                          <h2>Winner(s) for {result.role}</h2>
+                          {winners.map(winner => (
+                              <div key={winner.name}>
+                                  <img src={winner.img_URL} alt={winner.name} style={{ width: '100px', height: '100px' }}/>
+                                  <p>{winner.name}: {winner.votes_number} votes.</p>
+                                  <p>{winnerPercentage}% of total votes</p>
+                              </div>
+                          ))}
+                      </div>
+                  );
+              })
+            ) : (
+                data.map((result, index) => {
+                    const highestVote = Math.max(...result.candidates.map(candidate => candidate.votes_number));
+                    return (
+                        <div key={index}>
+                            <h2>Role: {result.role}</h2>
+                            <ResponsiveContainer width="100%" height={400}>
+                                <BarChart 
+                                    data={result.candidates} 
+                                    margin={{top: 15, right: 30, left: 20, bottom: 5}}>
+                                    <CartesianGrid strokeDasharray="3 3"/>
+                                    <XAxis dataKey="name"/>
+                                    <YAxis/>
+                                    <Tooltip/>
+                                    <Legend/>
+                                    <Bar dataKey="votes_number">
+                                        {
+                                            result.candidates.map((entry, index) => (
+                                                <Cell key={`cell-${index}`} fill={renderColorfulBar(entry, highestVote)} />
+                                            ))
+                                        }
+                                    </Bar>
+                                </BarChart>
+                            </ResponsiveContainer>
+                        </div>
+                    );
+                })
+            )}
         </div>
     );
 }
