bd45bfb1f23110c8ba41cf3209619d83369076d8
BenomarAchraf <101630642+BenomarAchraf@users.noreply.github.com>
2023-12-03 00:08:09 +0100

    asynchronous payment

---
+++ paiment-service/controllers/Async/subscriber.js
@@ -0,0 +1,48 @@
+const amqp = require("amqplib/callback_api");
+const paymentController = require("../PaymentController");
+const publishvote = require("./publisher");
+exports.subscribe = () => {
+  amqp.connect(`amqp://localhost`, (err, connection) => {
+    if (err) {
+      throw err;
+    }
+
+    connection.createChannel((err, chanel) => {
+      if (err) {
+        throw err;
+      }
+      let queueName = "PaymentAccount";
+      chanel.assertQueue(queueName, { durable: false });
+      chanel.consume(queueName, (msg) => {
+        const email = msg.content.toString();
+        console.log(`Received: ${msg.content.toString()}`);
+        paymentController.AsyncCreateAccount(email);
+        chanel.ack(msg);
+      });
+    });
+  });
+};
+
+
+exports.subscribeVote = () => {
+  amqp.connect(`amqp://localhost`, (err, connection) => {
+    if (err) {
+      throw err;
+    }
+
+    connection.createChannel((err, chanel) => {
+      if (err) {
+        throw err;
+      }
+      let queueName = "VoteQueue";
+      chanel.assertQueue(queueName, { durable: false });
+      chanel.consume(queueName, (msg) => {
+        console.log("Receive from the queue VoteQueue");
+        const email = msg.content.toString();
+        console.log(`email voter: ${msg.content.toString()}`);
+        paymentController.AsyncPay(email);
+        chanel.ack(msg);
+      });
+    });
+  });
+};
--- paiment-service/controllers/PaymentController.js
+++ paiment-service/controllers/PaymentController.js
@@ -1,5 +1,5 @@
 const Payment = require("../models/Payment");
-
+const publish = require("./Async/publisher");
 exports.createPaymentAccount = async (req, res) => {
   try {
     const { user_id, solde } = req.body;
@@ -44,3 +44,52 @@ exports.Payment = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+exports.AsyncCreateAccount = async (msg) => {
+  try {
+    const email = msg;
+    const newPayment = new Payment({
+      email,
+    });
+
+    await newPayment.save();
+
+    console.log({ message: "Your Bank Account has been created successfully" });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.AsyncPay = async (msg) => {
+  
+  try {
+    const email = msg;
+    const payment = await Payment.findOne({ email });
+    console.log("Payment process");
+
+    if (!payment) {
+      console.log("invalide email");
+      const msg="invalide email";
+      publish.publishvote(msg);
+    }
+    if (payment.solde - 50 < 0) {
+      console.log("Insufisant solde to do the action");
+      const msg="Insufisant solde to do the action";
+      publish.publishvote(msg);
+    } else {
+      let newsolde = payment.solde - 50;
+      payment.solde = newsolde;
+
+      await payment.save();
+      console.log("Your Action has been done successfully");
+      const msg = "Your Action has been done successfully";
+      publish.publishvote(msg);
+    }
+  } catch (error) {
+    const msg="Server error";
+      publish.publishvote(msg);
+  }
+};
+
+
